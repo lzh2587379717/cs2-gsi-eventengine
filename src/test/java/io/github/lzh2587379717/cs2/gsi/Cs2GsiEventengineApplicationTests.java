bc0bb2fc468e62fa808e1a12a84cd28160880ee6
LZH <186022561+lzh2587379717@users.noreply.github.com>
2026-04-18 09:38:59 +0800

    chore: :tada: bootstrap Spring Boot project
    - Add Gradle build configuration
    - Add Gradle Wrapper
    - Create application entrypoint
    - Add application configuration
    - Add unit test skeleton
    - Configure Git repository rules (.gitignore, .gitattributes)

---
+++ src/test/java/io/github/lzh2587379717/cs2/gsi/Cs2GsiEventengineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lzh2587379717.cs2.gsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2GsiEventengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
