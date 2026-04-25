85293716abccafca1b73f97e577c464dd442a38a
LZH <186022561+lzh2587379717@users.noreply.github.com>
2026-04-25 19:55:30 +0800

    chore: :bulb: add copyright headers to Java files
    - Apply MIT copyright header to all *.java files

--- src/test/java/io/github/lzh2587379717/cs2/gsi/Cs2GsiEventengineApplicationTests.java
+++ src/test/java/io/github/lzh2587379717/cs2/gsi/Cs2GsiEventengineApplicationTests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 LZH
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 package io.github.lzh2587379717.cs2.gsi;
 
 import org.junit.jupiter.api.Test;
