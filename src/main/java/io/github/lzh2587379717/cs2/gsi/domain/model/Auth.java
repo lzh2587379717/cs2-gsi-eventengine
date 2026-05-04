5d282b460ad8ae33a9a1cbf0876905e6aa40201e
LZH <186022561+lzh2587379717@users.noreply.github.com>
2026-05-04 21:51:57 +0800

    feat: :sparkles: add auth model
    - Add Auth model representing authentication

---
+++ src/main/java/io/github/lzh2587379717/cs2/gsi/domain/model/Auth.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 LZH
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
+package io.github.lzh2587379717.cs2.gsi.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Auth {
+
+	private String token;
+
+}
