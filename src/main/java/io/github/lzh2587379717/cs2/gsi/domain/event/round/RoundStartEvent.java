368b8515e61251b738874aaa925716a2399915b3
LZH <186022561+lzh2587379717@users.noreply.github.com>
2026-05-23 23:47:28 +0800

    feat: :sparkles: add core round phase event classes
    - Add RoundStartEvent class for round start detection
    - Add RoundEndEvent class for round end detection
    - Add package-info.java with @NullMarked for null-safety

---
+++ src/main/java/io/github/lzh2587379717/cs2/gsi/domain/event/round/RoundStartEvent.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026 LZH
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
+package io.github.lzh2587379717.cs2.gsi.domain.event.round;
+
+import io.github.lzh2587379717.cs2.gsi.domain.event.AbstractGsiEvent;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+public class RoundStartEvent extends AbstractGsiEvent {
+
+}
